aae9f7719a0c9ba95c2e92f97de647087188fa81
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-10-07 14:10:50 -0300

    submenuDOM

--- menu.js
+++ menu.js
@@ -7,3 +7,30 @@ hamburgerIcon.addEventListener('click', ()=> {
 
     navOverlay.classList.toggle('nav__overlay--show');
 })
+
+navOverlay.addEventListener('click', (e) => {
+    const currentElement = e.target;
+    e.preventDefault();
+    //console.log(e.target.classList.value);
+
+    if(isActive(currentElement, 'nav__parent') ){
+        
+        const subMenu = currentElement.parentElement.children[1];
+        console.log(subMenu);
+
+        if(window.innerWidth < 768){
+
+            let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
+
+            subMenu.style.height = `${height}px`;
+
+        }else{
+
+        }
+
+    }
+})
+
+function isActive(element, string){
+    return element.classList.value.includes(string);
+}
